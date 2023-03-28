58b55c525104462dad62ede4957d21bab58662de
凌楚 <yangkun.ayk@alibaba-inc.com>
2023-03-28 12:17:28 +0800

    Dev

--- src/main/java/io/github/aaronai/FoobarInstrumentation.java
+++ src/main/java/io/github/aaronai/FoobarInstrumentation.java
@@ -10,6 +10,7 @@ import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class FoobarInstrumentation implements TypeInstrumentation {
+
     private static final Logger logger = LoggerFactory.getLogger(FoobarInstrumentation.class);
 
     @Override
@@ -19,16 +20,21 @@ public class FoobarInstrumentation implements TypeInstrumentation {
 
     @Override
     public void transform(TypeTransformer transformer) {
+        // Apply advice to the "go" method of "io.github.aaronai.Foobar"
         transformer.applyAdviceToMethod(isMethod().and(named("go")), FoobarInstrumentation.class.getName() + "$GoAdvice");
     }
 
+    // Define the advice to be applied to the "go" method
     @SuppressWarnings("unused")
     public static class GoAdvice {
+
+        // This method will be invoked before the "go" method
         @Advice.OnMethodEnter(suppress = Throwable.class)
         public static void onStart() {
             logger.info("This method will be invoked before Foobar#go");
         }
 
+        // This method will be invoked after the "go" method
         @Advice.OnMethodExit(suppress = Throwable.class)
         public static void onExit() {
             logger.info("This method will be invoked after Foobar#go");
--- src/main/java/io/github/aaronai/Main.java
+++ src/main/java/io/github/aaronai/Main.java
@@ -1,7 +1,6 @@
 package io.github.aaronai;
 
 import java.lang.instrument.Instrumentation;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
 import net.bytebuddy.agent.ByteBuddyAgent;
@@ -12,16 +11,28 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(io.github.aaronai.Main.class);
 
+    /**
+     * Define a method that returns a list of TypeInstrumentation objects.
+     */
     @SuppressWarnings("ArraysAsListWithZeroOrOneArgument")
     public static List<TypeInstrumentation> typeInstrumentations() {
         return Arrays.asList(new FoobarInstrumentation());
     }
 
+    /**
+     * Define a premain method that will be called when the Java agent is started.
+     */
     public static void premain(String arguments, Instrumentation instrumentation) {
         logger.debug("start to execute premain");
+
+        // Create a new AgentBuilder object
         final AgentBuilder.Default agentBuilder = new AgentBuilder.Default();
+
+        // Iterate over the list of TypeInstrumentation objects
         for (TypeInstrumentation typeInstrumentation : typeInstrumentations()) {
-            final AgentBuilder.Identified.Extendable extendable = agentBuilder.type(typeInstrumentation.typeMatcher()).transform(ConstantAdjuster.instance());
+            // Get the typeMatcher from the TypeInstrumentation object and use it to create an extendable identified AgentBuilder
+            final AgentBuilder.Identified.Extendable extendable = agentBuilder.type(typeInstrumentation.typeMatcher())
+                .transform(ConstantAdjuster.instance());
             final TypeTransformerImpl transformer = new TypeTransformerImpl(extendable);
             typeInstrumentation.transform(transformer);
             transformer.getAgentBuilder().installOn(instrumentation);
@@ -29,8 +40,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        // Install the ByteBuddy agent
         ByteBuddyAgent.install();
+        // Get the instrumentation from the ByteBuddy agent
         final Instrumentation inst = ByteBuddyAgent.getInstrumentation();
+        // Call the premain method with an empty argument string and the obtained instrumentation
         Main.premain("", inst);
         final Foobar foobar = new Foobar();
         foobar.go();
