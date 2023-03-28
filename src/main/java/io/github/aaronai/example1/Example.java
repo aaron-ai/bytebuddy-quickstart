24e1060d13df8b7251f6615860a7ea0b4a11215a
凌楚 <yangkun.ayk@alibaba-inc.com>
2023-03-28 11:00:31 +0800

    Dev

--- src/main/java/io/github/aaronai/example1/Example.java
+++ src/main/java/io/github/aaronai/example1/Example.java
@@ -1,47 +1,43 @@
 package io.github.aaronai.example1;
 
-import io.github.aaronai.example0.Foo;
 import java.lang.instrument.Instrumentation;
 import java.lang.reflect.InvocationTargetException;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.matcher.ElementMatcher;
-import net.bytebuddy.matcher.ElementMatchers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Example {
-    public static void premain(String arguments, Instrumentation instrumentation) {
-        System.out.println("=================");
-        // 使用 ByteBuddy 修改字节码并增强目标应用程序
+    private static final Logger log = LoggerFactory.getLogger(Example.class);
 
+    public static void premain(String arguments, Instrumentation instrumentation) {
+        log.debug("start to execute premain");
         new AgentBuilder.Default()
             .type(named("io.github.aaronai.example1.Foobar")).transform(new AgentBuilder.Transformer.ForAdvice().advice(isMethod()
-                .and(named("go")), Example.class.getName() + "$GoAdvice")).installOn(instrumentation);
+                .and(named("go")), Example.class.getName() + "$GoAdvice"))
+            .installOn(instrumentation);
 
     }
 
     public static class GoAdvice {
         @Advice.OnMethodEnter(suppress = Throwable.class)
         public static void onStart() {
-            System.out.println("==========start==========");
+            log.info("enter go method");
         }
     }
 
     public static void main(
         String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        // 在应用程序启动之前，调用 ByteBuddyAgent.install() 方法来安装代理类
         ByteBuddyAgent.install();
         final Instrumentation inst = ByteBuddyAgent.getInstrumentation();
         Class.forName("io.github.aaronai.example1.Example", true, ClassLoader.getSystemClassLoader())
             .getDeclaredMethod("premain", String.class, Instrumentation.class)
             .invoke(null, "", inst);
-        System.out.println("++++++++++++++++++++");
         final Foobar foobar = new Foobar();
         foobar.go();
-        // 执行应用程序的 main 方法
     }
 }
--- src/main/java/io/github/aaronai/example1/Foobar.java
+++ src/main/java/io/github/aaronai/example1/Foobar.java
@@ -1,7 +1,12 @@
 package io.github.aaronai.example1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Foobar {
+    private static final Logger log = LoggerFactory.getLogger(Example.class);
+
     public void go() {
-        System.out.println("go");
+        log.info("This is Foobar#go");
     }
 }
